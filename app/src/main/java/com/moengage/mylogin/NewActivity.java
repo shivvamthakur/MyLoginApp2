ce2e7b3fb38d06ddacf37342385f54cf4ea6ee1e
Shivam Thakur <shivvamthakurr@gmail.com>
2023-05-15 02:35:43 +0530

    Initial commit

---
+++ app/src/main/java/com/moengage/mylogin/NewActivity.java
@@ -0,0 +1,37 @@
+package com.moengage.mylogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.button.MaterialButton;
+
+public class NewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+
+        MaterialButton logoutbtn = (MaterialButton) findViewById(R.id.logoutbtn);
+
+        logoutbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
